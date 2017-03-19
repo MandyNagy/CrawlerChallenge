efc33b7e5aa21f921388c569d4441a17cd61970d
MandyNagy <mandynagy4@gmail.com>
2017-03-20 00:25:21 +0100

    clean-ups and check links

--- src/main/java/crawler/interviewProject/Search.java
+++ src/main/java/crawler/interviewProject/Search.java
@@ -13,18 +13,39 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+/**
+ * Search of image urls.
+ * 
+ * @author mandy
+ *
+ */
 public class Search {
 
+	/**
+	 * The webdriver.
+	 */
 	public static WebDriver driver;
 
+	/**
+	 * The base url.
+	 */
 	public static final String url = "http://muenchen.bringmeister.de/obst-gemuse.html";
 
-	public static Writer writer;
-
+	/**
+	 * Response for tracking status codes.
+	 */
 	private static Response response;
+	
+	/**
+	 * Timer. 
+	 */
 	public static StopWatch pageLoad;
 
-	public static void XpathSearch_first60Prod() throws InterruptedException {
+	/**
+	 * Method in which the first visible 60 products are verified.
+	 * 
+	 */
+	public static void XpathSearch_first60Prod() {
 
 		driver = new FirefoxDriver();
 		pageLoad = new StopWatch();
@@ -32,9 +53,7 @@ public class Search {
 		System.out.println(pageLoad.getTime());
 		driver.get(url);
 		driver.findElement(By.cssSelector("#zip_code")).sendKeys("80796");
-		driver.findElement(By.cssSelector("button.bm_button:nth-child(4)"))
-				.click();
-
+		driver.findElement(By.cssSelector("button.bm_button:nth-child(4)")).click();
 		String path = "//div[@class='bm_image-wrapper']/a[@role='link']/img";
 		// driver.findElement(By.cssSelector("div.bm_image-wrapper > a[role=link] > img"));
 		List<WebElement> productImages_60 = driver.findElements(By.xpath(path));
@@ -43,6 +62,13 @@ public class Search {
 		}
 	}
 
+	/**
+	 * Main method.
+	 * Printing the first 120 image urls of the available products.
+	 * 
+	 * @throws InterruptedException
+	 * @throws FileNotFoundException
+	 */
 	public static void XpathSearch_first120Prod() throws InterruptedException,
 			FileNotFoundException {
 
@@ -71,6 +97,28 @@ public class Search {
 				+ " image urls of products available.");
 	}
 
+	/**
+	 * Check all the links of the web page.
+	 */
+	public static void checkLinks() {
+		// Get all the links on the page
+		List<WebElement> links = driver.findElements(By.cssSelector("a"));
+
+		String href;
+
+		for (WebElement link : links) {
+			href = link.getAttribute("href");
+			response = given().get(href).then().extract().response();
+
+			if (response.getStatusCode() >= 400
+					&& response.getStatusCode() <= 599) {
+				System.out.println(href + " - response code : "
+						+ response.getStatusCode());
+				System.out.print(Thread.currentThread().getStackTrace());
+			}
+		}
+	}
+	
 	public static void main(String[] args) throws InterruptedException,
 			FileNotFoundException {
 
@@ -78,5 +126,6 @@ public class Search {
 
 		XpathSearch_first120Prod();
 
+		checkLinks();
 	}
 }
